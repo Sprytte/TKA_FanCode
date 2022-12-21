fbeefb8130112bc36de4f9d9b13e782fefdbec50
Émilie Parent <102536784+Sprytte@users.noreply.github.com>
2022-12-21 14:57:35 -0500

    story api works!!

--- src/main/java/com/example/tka_fancode/Request/StoryRequest.java
+++ src/main/java/com/example/tka_fancode/Request/StoryRequest.java
@@ -1,6 +1,7 @@
 package com.example.tka_fancode.Request;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.NotFound;
@@ -8,13 +9,19 @@ import org.hibernate.annotations.NotFound;
 @Getter
 @Setter
 public class StoryRequest {
-    //@NotBlank
+    @NotBlank
     private String adaptationName;
+    @NotBlank
     private String briefSummary;
+    @NotBlank
     private double rating;
+    @NotBlank
     private String creators;
+    @NotBlank
     private String length;
+    @NotBlank
     private String fullSummary;
+    @NotBlank
     private String extraInformation;
 
     /*@NotNull
