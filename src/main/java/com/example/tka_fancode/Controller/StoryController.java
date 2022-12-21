c0091df5ae9c857f37bd853c5633780328ee2e4b
Émilie Parent <102536784+Sprytte@users.noreply.github.com>
2022-12-21 14:18:22 -0500

    full story api

--- src/main/java/com/example/tka_fancode/Controller/StoryController.java
+++ src/main/java/com/example/tka_fancode/Controller/StoryController.java
@@ -1,81 +1,50 @@
 package com.example.tka_fancode.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.tka_fancode.Entities.Story;
+import com.example.tka_fancode.Request.StoryRequest;
+import com.example.tka_fancode.Response.StoryResponse;
+import com.example.tka_fancode.Service.StoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("TKA/Fan_Page")
+@RequestMapping("TKA/Adaptations")
 public class StoryController {
-}
-
-/*@RestController
-@CrossOrigin("*")
-@RequestMapping("/api/teachers")
-public class TeacherController {
     @Autowired
-    TeacherService teacherService;
+    StoryService storyService;
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{teacher_id}/courses")
-    public CourseResponse addCourse(
-            @PathVariable long teacher_id,
-            @Valid @RequestBody CourseRequest courseRequest
-            ){
-        return new CourseResponse(teacherService.addCourse(teacher_id, courseRequest));
-    }
-
-    @GetMapping("/{teacherId}/courses")
-    public List<CourseResponse> getAllCourses(@PathVariable long teacherId){
-        List<Course> courses = teacherService.getAllCourses(teacherId);
-        List<CourseResponse> courseResponses = new ArrayList<>();
-        for(int i=0; i < courses.size(); i++){
-            courseResponses.add(new CourseResponse(courses.get(i)));
-        }
-
-        return courseResponses;
-    }
-
-    @DeleteMapping("/{teacherId}/courses")
-    public void deleteAllCourses(@PathVariable long teacherId){
-        teacherService.deleteAllCourses(teacherId);
-    }
     @GetMapping()
-    public List<TeacherResponse> getAllTeachers(@RequestParam(required = false) String firstName){
-
-        List<Teacher> teachers = teacherService.getAllTeachers(firstName);
-        List<TeacherResponse> teacherResponses = new ArrayList<>();
-        teachers.forEach(teacher -> {
-            TeacherResponse teacherResponse = new TeacherResponse(teacher);
-            teacherResponses.add(teacherResponse);
+    public List<StoryResponse> getAllStories(@RequestParam(required = false) String adaptationName){
+        List<Story> stories = storyService.getAllStories(adaptationName);
+        List<StoryResponse> storyResponses = new ArrayList<>();
+        stories.forEach(story -> {
+            StoryResponse storyResponse = new StoryResponse(story);
+            storyResponses.add(storyResponse);
         });
-        return teacherResponses;
+        return storyResponses;
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public TeacherResponse addTeacher(@Valid @RequestBody TeacherRequest teacherRequest)
-    {
-
-            Teacher savedTeacher = teacherService.addTeacher(teacherRequest);
-
-            return new TeacherResponse(savedTeacher);
-
+    public StoryResponse addStory(@Valid @RequestBody StoryRequest storyRequest){
+        Story savedStory = storyService.addStory(storyRequest);
+        return new StoryResponse(savedStory);
     }
 
-    @PutMapping("/{id}")
-    public TeacherResponse updateTeacher
-            (@PathVariable long id,
-             @Valid @RequestBody TeacherRequest teacherRequest){
-
-        Teacher updatedTeacher = teacherService.updateTeacher(id, teacherRequest);
-        return new TeacherResponse(updatedTeacher);
+    @PutMapping("/id")
+    public StoryResponse updateStory(@PathVariable long id, @Valid @RequestBody StoryRequest storyRequest){
+        Story updatedStory = storyService.updateStory(id, storyRequest);
+        return new StoryResponse(updatedStory);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTeacher(@PathVariable long id)
-    {
-        teacherService.deleteTeacher(id);
+    public void deleteStory(@PathVariable long id){
+        storyService.deleteStory(id);
     }
-}*/
+}
